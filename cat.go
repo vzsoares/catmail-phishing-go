535d5e8b2dfb05d001e66395c53f556d3b7f2bb8
vzsoares <jogolots2@gmail.com>
2024-04-21 13:37:31 -0300

    parallel fetch

--- cat.go
+++ cat.go
@@ -6,40 +6,67 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sync"
 )
 
 const CAT_API = "https://api.thecatapi.com/v1/images/search?mime_types=jpg"
+const N = 10
 
 type Cache map[string]bool
 
 func main() {
-
+	urlChan := getImageUrls()
 	fmt.Println("hallo")
 	// TODO use in disk cache
-	var cache Cache = make(map[string]bool)
-	for i := range 100 {
-		getImageUrl(cache)
-		fmt.Println(i)
+	i := 0
+	for url := range urlChan {
+		fmt.Println(url)
+		i++
 	}
-	getImageUrl(cache)
+	fmt.Println("successfuly fetched", i, "urls")
+}
+func getImageUrls() <-chan string {
+	var cache Cache = make(map[string]bool)
+	out := make(chan string)
+	wg := sync.WaitGroup{}
+
+	action := func() {
+		defer wg.Done()
+		url, _ := getImageUrl(cache)
+		if len(url) > 1 {
+			out <- url
+		}
+
+	}
+
+	for i := 0; i < N; i++ {
+		wg.Add(1)
+		go action()
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+
 }
 func getImageUrl(cache Cache) (string, error) {
 	response, err := http.Get(CAT_API)
 	if err != nil {
-		fmt.Printf("error getting image res %s", err)
+		fmt.Printf("error getting image res %s\n", err)
 		return "", err
 	}
 
-    defer response.Body.Close()
+	defer response.Body.Close()
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		fmt.Printf("error getting image body %s", err)
+		fmt.Printf("error getting image body %s\n", err)
 		return "", err
 	}
 	var data [](map[string]interface{})
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		fmt.Printf("error parsing json  %s", err)
+		fmt.Printf("error parsing json  %s\n", err)
 		return "", err
 	}
 	var imgUrl string = data[0]["url"].(string)
@@ -51,6 +78,6 @@ func getImageUrl(cache Cache) (string, error) {
 		return "", errors.New("already fetched")
 	}
 	cache[id] = true
-	fmt.Printf("%v %v", id, imgUrl)
+	//fmt.Printf("%v %v", id, imgUrl)
 	return imgUrl, nil
 }
